af67f83411a530d02ffa72af0cbbd30f64a4c82f
thjkral <thjkral@gmail.com>
2024-01-03 21:00:15 +0100

    Added Python files I need for now

---
+++ src/dataLoader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Get the requested data from its original source
+"""
+
+import pandas
+import requests
+import json
+import logging
+
+def get_migration_data():
+    return null
+
---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Main Python script
+"""
+
+# import modules
+import pandas
+import requests
+import argparse
+import logging
+
+# import Python scripts
+import dataLoader
+
+
+
